e75abd9dc813b9a32fbaac5bbaadf667262a3d80
Vincent Siebert <vincent@siebert.im>
2012-08-06 18:55:19 +0100

    adding prezto

---
+++ prezto.rb
@@ -0,0 +1,12 @@
+dep 'prezto', :argument do 
+  # requires 'xcode tools'
+  met? { which 'bla' }
+  meet do
+    shell "git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.oh-my-zsh"
+    Dir["#{ENV['HOME']}/.oh-my-zsh/runcoms/z{shenv,shrc,login,logout}"].each do |path|
+      shell "cp -f #{path} #{ENV['HOME']}/#{File.basename path}"
+    end
+    shell "chsh -s /bin/zsh"
+    #shell "chmod ugo-x /usr/libexec/path_helper" if host.osx?
+  end
+end
