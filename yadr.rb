fae5385ad360e4b1cb9759a5b5fe91bfcea22675
Vincent Siebert <vincent@siebert.im>
2012-08-08 10:40:24 +0100

    updating yadr installer

--- yadr.rb
+++ yadr.rb
@@ -8,27 +8,34 @@ dep 'yadr', :argument, :on => :osx do
 
   path = "#{ENV['HOME']}/.yadr"
 
-  met? { false } #path.p.exist? }
+  met? { path.p.exist? }
 
   meet do
     system "git clone https://github.com/skwp/dotfiles #{path}"
     # rake install has to be called after cd to make sure ENV[PWD]
     # points to the correct .yadr dir. Using babushka's cd cmd would
     # not set the ENV[PWD] value to the dir cd'ed into
-    # system "cd #{path} && rake install"
+    system "cd #{path} && rake install"
   end
 
   after do
-    system "rm -rf #{path}/custom" if "#{path}/custom".p.exist?
-    custom_repo ||= get_value "Please specigy your yadr custom repository:"
-    system "git clone https://github.com/luxerama/yadr-customisations.git #{path}/custom"
-    cd "#{path}/custom" do |custom_path|
-      Dir["#{custom_path}/vim/vimrc.{before,after}"].each do |vimrc_path|
-        target_path = "~/.#{File.basename vimrc_path}"
-        if target_path.p.exist?
-          shell "unlink #{target_path}" if confirm "#{target_path} exists, overwrite?"
+    custom_repo ||= Babushka::Prompt.get_value "Please specify your yadr custom repository (optional)", { :prompt => ":"}, &Proc.new { |value|
+      log_shell "Removing default custom directory", "rm -rf #{path}/custom" if "#{path}/custom".p.exist?
+      begin
+        shell! "git clone #{value} #{path}/custom"
+      rescue Exception
+        false
+      end
+    }
+    if "#{path}/custom".p.exist?
+      cd "#{path}/custom" do |custom_path|
+        Dir["#{custom_path}/vim/vimrc.{before,after}"].each do |vimrc_path|
+          target_path = "~/.#{File.basename vimrc_path}"
+          if target_path.p.exist?
+            shell "unlink #{target_path}" if confirm "#{target_path} exists, overwrite?"
+          end
+          log_shell "Linking vim config to #{target_path}", "ln -s #{vimrc_path} #{target_path}"
         end
-        system "ln -s #{vimrc_path} $tar" 
       end
     end
   end
